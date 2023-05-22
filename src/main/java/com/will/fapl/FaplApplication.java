aea4ecf138d8fd4ea4e9d789bbba76310f4e8841
will kim <78planet@gmail.com>
2023-05-22 13:49:37 +0900

    chore: project initial setting

---
+++ src/main/java/com/will/fapl/FaplApplication.java
@@ -0,0 +1,13 @@
+package com.will.fapl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaplApplication.class, args);
+	}
+
+}
