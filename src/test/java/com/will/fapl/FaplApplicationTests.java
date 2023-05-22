180a87ad662595f566342a3112a4ecf528614a04
will kim <78planet@gmail.com>
2023-05-22 14:41:35 +0900

    #1 project initial setting (#9)
    * chore: project initial setting
    
    * chore: add google coding convention
    
    * chore: modify build.gradle

---
+++ src/test/java/com/will/fapl/FaplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.fapl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
