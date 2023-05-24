761a8a1d72f82e5de54efd52ee0d7c6a2198acd9
will kim <78planet@gmail.com>
2023-05-24 18:22:59 +0900

    feat: modify sql

--- src/main/resources/db/migration/V2__rename.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE comment RENAME COLUMN class TO hierarchy;
-ALTER TABLE post_like_users RENAME post_like_user;
-ALTER TABLE post_dislike_users RENAME post_dislike_user;
-ALTER TABLE follows RENAME follow;
--- src/main/resources/db/migration/V3__Modify_point.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE point DROP user_id;
-ALTER TABLE `user` ADD point_id integer NOT NULL;
-ALTER TABLE `user` ADD FOREIGN KEY (`point_id`) REFERENCES `point` (`id`);
